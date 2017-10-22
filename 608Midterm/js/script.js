36194e2c882b8cb4bd3801e39faf7d2956ca729b
Jumi Yoon <design.jumi@gmail.com>
2017-10-22 12:05:01 -0700

    navbar-dropdown
    Former-commit-id: ee2a515b1d7e52bf813b58b375df33415538cfa5
    Former-commit-id: f92ecec1982cbcf46d794cebe3d0426eab92af8f

--- 608Midterm/js/script.js
+++ 608Midterm/js/script.js
@@ -28,5 +28,5 @@ $(document).ready(function() {
 
     $(document).scroll(function() {
         var $nav = $(".navbar.fixed-top");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $nav.toggleClass('scrolled', $(this).scrollTop() > 0);
     });
