25f1a64dbd2c557346395ae953d7a7ffca15421b
Jumi Yoon <design.jumi@gmail.com>
2017-10-21 23:12:32 -0700

    Added a navbar scroll effect base code.
    Former-commit-id: bc8f0253aaeeee9a4d880d064edcef4b6ea0be53
    Former-commit-id: d547833859fe34be45501bb92d8bda165329f3ec

--- 608Midterm/js/script.js
+++ 608Midterm/js/script.js
@@ -1,27 +1,32 @@
-$(document).ready(function(){
-  $('.qty-change-minus').click(function(e) {
-  	    // Stop acting like a button
+$(document).ready(function() {
+    $('.qty-change-minus').click(function(e) {
+        // Stop acting like a button
         e.preventDefault();
         // Get the field name
         var quantity = parseInt($('#qty-input').val());
         // If is not undefined
         if (quantity > 0) {
-	        $('#qty-input').val(quantity - 1);
+            $('#qty-input').val(quantity - 1);
         }
-  });
+    });
 
-  $('.qty-change-plus').click(function(e) {
-  	    // Stop acting like a button
+    $('.qty-change-plus').click(function(e) {
+        // Stop acting like a button
         e.preventDefault();
         // Get the field name
         var quantity = parseInt($('#qty-input').val());
         // If is not undefined
         $('#qty-input').val(quantity + 1);
-  });
+    });
 
-$('#exampleModalLongTitle').on('shown.bs.modal', function () {
-  $('#myInput').focus();
-})
+    $('#exampleModalLongTitle').on('shown.bs.modal', function() {
+        $('#myInput').focus();
+    })
+
 
 });
 
+    $(document).scroll(function() {
+        var $nav = $(".navbar.fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
